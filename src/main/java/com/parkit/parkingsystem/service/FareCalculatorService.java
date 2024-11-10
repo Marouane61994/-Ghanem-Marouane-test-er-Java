2077898ff7a1f3017dceb5b67a74b812d47a6715
Marouane61994 <marouanneghanem@hotmail.fr>
2024-11-10 19:18:21 +0100

    Step 1: Unit tests will run without errors

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -9,13 +9,13 @@ public class FareCalculatorService {
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
-
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
-
+        // Calculer la durée en millisecondes
+        long inTimeMillis = ticket.getInTime().getTime();
+        long outTimeMillis = ticket.getOutTime().getTime();
+        // Convertir la durée en heures
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
-
+        double duration = (outTimeMillis - inTimeMillis)/(1000.0*60*60);// Diviser pour obtenir la durée en heures
+        // Appliquer les tarifs en fonction du type de véhicule
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
                 ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
